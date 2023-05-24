ed4c0a14ef9aa110fac75bf92423a0b558ece9e8
pwnh4 (@loicttn) <loic@ttn.am>
2023-05-24 14:15:59 +0200

    nt

---
+++ src/Hello.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Hello {
+    bool public world = false;
+
+    function hello() public {
+        world = true;
+    }
+}
